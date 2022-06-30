6005eef3fbe691640e03dfe96789809b2e8b2628
Alec Delaney <tekktrik@gmail.com>
2022-06-30 00:09:44 -0400

    Add tools install script

--- install_tools.sh
+++ install_tools.sh
@@ -1,5 +1,13 @@
-for f in ./submodules/adabot/tools/*.py ; do
-    filename="$(basename $f)"
-    echo "$filename"
-    ln  ./submodules/adabot/tools/$filename ./toml_switcher/$filename;
-done
+if [ "$1" == "install" ]; then
+    for f in ./submodules/adabot/tools/*.py; do
+        filename="$(basename $f)"
+        echo "Copying $filename to toml_switcher/"
+        ln  ./submodules/adabot/tools/$filename ./toml_switcher/$filename;
+    done
+elif [ "$1" == "clean" ]; then
+    for f in ./toml_switcher/*.py; do
+        filename="$(basename $f)"
+        echo "Deleting $filename in toml_switcher/"
+        rm ./toml_switcher/$filename;
+    done
+fi
