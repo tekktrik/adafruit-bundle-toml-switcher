cd58462650760af6655d0138fca77b514e45fc91
Alec Delaney <tekktrik@gmail.com>
2022-06-30 00:26:14 -0400

    Isntall only specific files from adabot/tools

--- install_tools.sh
+++ install_tools.sh
@@ -1,13 +1,9 @@
-if [ "$1" == "install" ]; then
-    for f in ./submodules/adabot/tools/*.py; do
-        filename="$(basename $f)"
+while read filename; do
+    if [ "$1" == "install" ]; then
         echo "Copying $filename to toml_switcher/"
         ln  ./submodules/adabot/tools/$filename ./toml_switcher/$filename;
-    done
-elif [ "$1" == "clean" ]; then
-    for f in ./toml_switcher/*.py; do
-        filename="$(basename $f)"
+    elif [ "$1" == "clean" ]; then
         echo "Deleting $filename in toml_switcher/"
         rm ./toml_switcher/$filename;
-    done
-fi
+    fi
+done < tools_reqs.txt
