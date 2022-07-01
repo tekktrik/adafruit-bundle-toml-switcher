beedf31839e422ddf4a85dc5b26516f797578b0f
Alec Delaney <tekktrik@gmail.com>
2022-07-01 11:05:16 -0400

    Add scripts for getting dependencies

---
+++ toml_switcher/get_dependencies.py
@@ -0,0 +1,106 @@
+# SPDX-FileCopyrightText: 2022 Alec Delaney, written for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+from typing import Any
+import os
+import typer
+from iterate_libraries import iter_local_bundle_with_func, LocalLibFunc_IterResult
+from library_functions import StrPath
+from setup_py_parser import parse_setup_py
+
+app = typer.Typer()
+
+@app.command()
+def check_dependencies(output_dir: str = "reqlists", bundle_path: str = "submodules/bundle") -> None:
+    """Check the ``requirements.txt`` and ``setup.py`` dependencies and record them.
+    If they don't match, record both notify the user
+    
+    :param str output_dir: The output directory filepath
+    :param str bundle_path: The filepath to the bundle directory
+    """
+
+    results = check_all_repo_deps(bundle_path)
+
+    os.makedirs(output_dir)
+
+    for result in results:
+        lib_path, lib_results = result
+        reqs_dict = lib_results[0]
+        req_base = os.path.join(output_dir, os.path.basename(lib_path))
+        if "union" in reqs_dict:
+            req_file = req_base + "_req.txt"
+            with open(req_file, mode="w", encoding="utf-8") as reqfile:
+                reqfile.write("".join([req + "\n" for req in reqs_dict["union"]]))
+        else:
+            os.mkdir(req_base)
+            req_file = os.path.join(req_base, os.path.basename(req_base)) + "_req.txt"
+            with open(req_file, mode="w", encoding="utf-8") as reqfile:
+                reqfile.write("### setup.py ###\n")
+                reqfile.write("".join([req + "\n" for req in reqs_dict["setup.py"]]))
+                reqfile.write("=========================================\n")
+                reqfile.write("### requirements.txt ###\n")
+                reqfile.write("".join([req + "\n" for req in reqs_dict["requirements.txt"]]))
+
+def check_repo_deps(lib_path: StrPath) -> dict[str, list[str]]:
+    """Check the dependencies of a given repository
+    
+    :param Repository lib_repo: The GitHub repository object
+    """
+
+    setup_py_fp = os.path.join(lib_path, "setup.py")
+    req_txt_fp = os.path.join(lib_path, "requirements.txt")
+
+    try:
+        with open(setup_py_fp, mode="r", encoding="utf-8") as setuppy:
+            setup_py_contents = setuppy.read()
+
+        setup_py_vars = parse_setup_py(setup_py_contents, ["install_requires"])
+        setup_py_set: set[str] = set(setup_py_vars["install_requires"])
+    except FileNotFoundError:
+        setup_py_set = None
+
+    with open(req_txt_fp, mode="r",encoding="utf-8") as reqtxt:
+        requirements_txt_set: set[str] = set([line.strip() for line in reqtxt if line.strip() and not line.strip().startswith("#")])
+
+    if setup_py_set:
+        parsed_setup_set = set()
+        for req in setup_py_set:
+            if req != "Adafruit-Blinka":
+                parsed_setup_set.add(req.lower().replace("_", "-"))
+            else:
+                parsed_setup_set.add(req)
+        setup_py_set = parsed_setup_set
+
+    if requirements_txt_set:
+        parsed_req_set = set()
+        for req in requirements_txt_set:
+            if req != "Adafruit-Blinka":
+                parsed_req_set.add(req.lower().replace("_", "-"))
+            else:
+                parsed_req_set.add(req)
+        requirements_txt_set = parsed_req_set
+
+    if setup_py_set and setup_py_set != requirements_txt_set:
+        # setup != req
+        # Write setup.py AND req.txt
+        return {
+            "requirements.txt": requirements_txt_set,
+            "setup.py": setup_py_set,
+        }
+    else:
+        # Only req
+        # Write req.txt
+        return {"union": requirements_txt_set}
+
+
+def check_all_repo_deps(bundle_path: str) -> list[LocalLibFunc_IterResult[dict[str, Any]]]:
+    """Check all bundles in the repo
+    
+    :param str bundle_path: The filepath to the bundle"""
+
+    return iter_local_bundle_with_func(bundle_path, [(check_repo_deps, (), {})])
+
+
+if __name__ == "__main__":
+    app()
---
+++ toml_switcher/setup_py_parser.py
@@ -0,0 +1,73 @@
+# SPDX-FileCopyrightText: 2022 Alec Delaney, written for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+Thanks to the Dependabot Core team, for the inspiration/idea on how to
+parse the ``setup.py`` file.
+"""
+
+import io
+import re
+from typing import Any
+import setuptools
+import pprint
+
+value_dict = {}
+
+def parse_setup_py(file_contents: str, keys: list[str]) -> dict[str, Any]:
+
+    global value_dict
+    value_dict = {}
+
+    def setup(*args, **kwargs):
+        """Fake ``setup()`` method"""
+        for key in keys:
+            value_dict[key] = kwargs.get(key)
+
+    setuptools.setup = setup
+
+    def noop(*args, **kwargs):
+        pass
+
+    def fake_parse(*args, **kwargs):
+        return []
+
+    global fake_open
+
+    def fake_open(*args, **kwargs):
+        content = (
+            "VERSION = ('0', '0', '1+dependabot')\n"
+            "__version__ = '0.0.1+dependabot'\n"
+            "__author__ = 'someone'\n"
+            "__title__ = 'something'\n"
+            "__description__ = 'something'\n"
+            "__author_email__ = 'something'\n"
+            "__license__ = 'something'\n"
+            "__url__ = 'something'\n"
+        )
+        return io.StringIO(content)
+
+    # Remove `print`, `open`, `log` and import statements
+    file_contents = re.sub(r"print\s*\(", "noop(", file_contents)
+    file_contents = re.sub(r"log\s*(\.\w+)*\(", "noop(", file_contents)
+    file_contents = re.sub(r"\b(\w+\.)*(open|file)\s*\(", "fake_open(", file_contents)
+    file_contents = file_contents.replace("parse_requirements(", "fake_parse(")
+    version_re = re.compile(r"^.*import.*__version__.*$", re.MULTILINE)
+    file_contents = re.sub(version_re, "", file_contents)
+
+    # Set variables likely to be imported
+    __version__ = "0.0.1+dependabot"
+    __author__ = "someone"
+    __title__ = "something"
+    __description__ = "something"
+    __author_email__ = "something"
+    __license__ = "something"
+    __url__ = "something"
+
+
+    __name__ = "__main__"
+
+    exec(file_contents)
+
+    return value_dict
